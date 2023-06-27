cef31023b28464815182685115b0f0440d21d3db
Dhiraj Shastri <99674211+dhiraj01-lang@users.noreply.github.com>
2023-06-27 11:34:09 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,11 @@
+public class DataType {
+    public static void main( String args[]){
+        int a = 10;
+        System.out.println(a);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean b = true;
+        System.out.println(b);
+    }
+    
+}
