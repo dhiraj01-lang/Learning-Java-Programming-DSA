87f1910ec22ca1e26b6f187c9b1b1eada19dc63f
Dhiraj Shastri <99674211+dhiraj01-lang@users.noreply.github.com>
2023-06-29 12:25:41 +0530

    Add files via upload

---
+++ InputinJava.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class InputinJava{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        //int b = sc.next();
+        //int sum = a+b;
+        System.out.println(input);
+    
+    }
+}
+
---
+++ input.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class input {
+    public static void main( String arg[]){
+        Scanner sc = new Scanner(System.in);
+        //Sum of a and b
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        //Product of a and b
+        // int a  = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product = a*b;
+        // System.out.println(product);
+
+        //Area of Circle
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+    
+}
