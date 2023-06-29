eee92e43854d32071234003456335f6d2302b383
Dhiraj Shastri <99674211+dhiraj01-lang@users.noreply.github.com>
2023-06-29 12:27:38 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TypeCasting {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float a = 24.33f;
+        int b = (int) a;
+        System.out.println(b);
+    }
+    
+}
---
+++ TypePromotion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TypePromotion {
+    public static void main(String args[]) {
+        int a = 10;
+        float b = 12.05f;
+        long c = 34;
+        double d = 32;
+        double ans = (a+b+c+d);
+        System.out.println(ans);
+        
+    }
+    
+}
