ef9832501b5d4976cb480bf13f64db37a1967531
Dhiraj Shastri <99674211+dhiraj01-lang@users.noreply.github.com>
2023-06-30 12:31:43 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,16 @@
+public class Operators{
+    public static void main(String args[]){
+        //Pre decrement
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        //Post decrement
+        int a = 10;
+        int b = a--;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ Practice02.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Practice02{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //Average of three numbers
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int avg = (a+b+c)/3;
+        // System.out.println(avg);
+
+        //AREA OF A SQUARE
+        // int side = sc.nextInt();
+        // int ans = side*side;
+        // System.out.println(ans);
+
+        //TOTAL COST WITH 18% GST
+        // float pen = sc.nextFloat();
+        // float pencil = sc.nextFloat();
+        // float eraser = sc.nextFloat();
+        // float total = (pen+pencil+eraser);
+        // System.out.println(total);
+
+        // float newTotal = total + (0.18f*total);
+        // System.out.println(newTotal);
+
+        int $ = 24;
+        System.out.println($);
+    }
+}
